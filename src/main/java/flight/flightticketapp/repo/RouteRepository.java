d8e9b65d692823d281cfb727b19462bf3f778155
muratakbyk <murathanakbyk.gmail.com>
2022-04-08 02:52:06 +0300

    updated

--- src/main/java/flight/flightticketapp/repo/RouteRepository.java
+++ src/main/java/flight/flightticketapp/repo/RouteRepository.java
@@ -17,12 +17,5 @@ public interface RouteRepository extends JpaRepository<Route, Integer> {
     List<Route>findByDepartureAirportIdAndAndArrivalAirportId(int departureAirportId,int arrivalAirportId);
 
 
-    // @Query(value="SELECT a FROM Route a WHERE  a.arrivalAirport.id = ?1")
-    //List<Route> findByArrivalAirportId(int arrivalAirportId);
 
-    //  @Query(value="SELECT a FROM Route a WHERE  a.departureAirport.id = ?1")
-    // List<Route> findByDepartureAirportId(int departureAirportId);
-
-    //  @Query(value="SELECT a FROM Route a WHERE  a.departureAirport.id = ?1 AND a.arrivalAirport.id=?2")
-    // List<Route>findByDepartureAndArrivalAirportId(int departureAirportId, int arrivalAirportId);
 }
